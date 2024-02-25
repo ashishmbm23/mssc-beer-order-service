c551476006b7e422708aa475787c327358369ca1
Ashish Verma <32788353+ashishmbm23@users.noreply.github.com>
2024-02-25 18:48:58 +0530

    Initial draft

---
+++ src/main/java/com/ashish/mssc/beerordeservice/BeerOrdeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ashish.mssc.beerordeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerOrdeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerOrdeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashish/mssc/beerordeservice/BeerOrdeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.mssc.beerordeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerOrdeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
