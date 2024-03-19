b0fee825860038c021581fbfa59c29b4c7a646e6
Ashish Verma <32788353+ashishmbm23@users.noreply.github.com>
2024-03-20 05:27:20 +0530

    Customer id coming as null in order response.

--- src/main/java/com/ashish/mssc/beerordeservice/web/mapper/BeerOrderMapper.java
+++ src/main/java/com/ashish/mssc/beerordeservice/web/mapper/BeerOrderMapper.java
@@ -3,9 +3,12 @@ package com.ashish.mssc.beerordeservice.web.mapper;
 import com.ashish.mssc.beerordeservice.domain.BeerOrder;
 import com.ashish.mssc.beerordeservice.web.model.BeerOrderDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper( uses = {DateMapper.class, BeerOrderLineMapper.class})
 public interface BeerOrderMapper {
+
+    @Mapping(target = "customerId", source = "customer.id")
     BeerOrderDto beerOrderToDto(BeerOrder beerOrder);
 
     BeerOrder dtoToBeerOrder(BeerOrderDto dto);
