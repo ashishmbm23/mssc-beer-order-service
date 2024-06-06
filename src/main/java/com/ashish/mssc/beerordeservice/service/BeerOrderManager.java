cfb1180dbf56061af601e805e0ebbedefc7efbe4
Ashish Verma <32788353+ashishmbm23@users.noreply.github.com>
2024-06-06 09:36:42 +0530

    Implemented state machine factory

---
+++ src/main/java/com/ashish/mssc/beerordeservice/service/BeerOrderManager.java
@@ -0,0 +1,8 @@
+package com.ashish.mssc.beerordeservice.service;
+
+import com.ashish.mssc.beerordeservice.domain.BeerOrder;
+
+public interface BeerOrderManager {
+
+    BeerOrder newBeerOrder(BeerOrder beerOrder);
+}
---
+++ src/main/java/com/ashish/mssc/beerordeservice/service/BeerOrderManagerImpl.java
@@ -0,0 +1,55 @@
+package com.ashish.mssc.beerordeservice.service;
+
+import com.ashish.mssc.beerordeservice.domain.BeerOrder;
+import com.ashish.mssc.beerordeservice.domain.BeerOrderEventEnum;
+import com.ashish.mssc.beerordeservice.domain.BeerOrderStatusEnum;
+import com.ashish.mssc.beerordeservice.repository.BeerOrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BeerOrderManagerImpl implements BeerOrderManager {
+
+    private final StateMachineFactory<BeerOrderStatusEnum, BeerOrderEventEnum> stateMachineFactory ;
+    private final BeerOrderRepository beerOrderRepository;
+
+    @Override
+    public BeerOrder newBeerOrder(BeerOrder beerOrder) {
+        beerOrder.setId(null);
+        beerOrder.setOrderStatus(BeerOrderStatusEnum.NEW);
+        BeerOrder savedBeerOrder = beerOrderRepository.save(beerOrder);
+        sendBeerOrderEvent(savedBeerOrder, BeerOrderEventEnum.VALIDATE_ORDER);
+        return savedBeerOrder;
+    }
+
+    //get state machine and send message
+    private void sendBeerOrderEvent(BeerOrder beerOrder, BeerOrderEventEnum eventEnum){
+        StateMachine<BeerOrderStatusEnum, BeerOrderEventEnum> stateMachine = build(beerOrder);
+        Message message = MessageBuilder.withPayload(eventEnum).build();
+        stateMachine.sendEvent(message);
+    }
+
+    //get state machine from state machine factory
+    private StateMachine<BeerOrderStatusEnum,BeerOrderEventEnum> build(BeerOrder beerOrder){
+        StateMachine<BeerOrderStatusEnum,BeerOrderEventEnum> stateMachine = stateMachineFactory.getStateMachine
+                (beerOrder.getId());
+
+        stateMachine.stop();
+
+        stateMachine.getStateMachineAccessor()
+                .doWithAllRegions( sma -> {
+                    sma.resetStateMachine( new DefaultStateMachineContext<>(beerOrder.getOrderStatus(), null, null, null));
+                });
+
+        stateMachine.start();
+
+        return stateMachine;
+
+    }
+}
